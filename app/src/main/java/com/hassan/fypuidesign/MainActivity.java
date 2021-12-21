1be072ba2a22de0ba88b881282f5295683b050cb
Haris_cse <muhammadhariskhancsemardan@gmail.com>
2021-12-21 14:29:22 +0500

    the lifecycle aware all events are added to the main activity

--- app/src/main/java/com/hassan/fypuidesign/MainActivity.java
+++ app/src/main/java/com/hassan/fypuidesign/MainActivity.java
@@ -10,5 +10,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getLifecycle().addObserver(new lifecycleaware());
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+
+        super.onStop();
     }
 }
--- app/src/main/java/com/hassan/fypuidesign/lifecycleaware.java
+++ app/src/main/java/com/hassan/fypuidesign/lifecycleaware.java
@@ -1,11 +1,36 @@
 package com.hassan.fypuidesign;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
 public class lifecycleaware implements LifecycleObserver {
-
-
-
+    private String TAG=this.getClass().getSimpleName();
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    void onCreate(){
+        Log.i(TAG, "onCreate: observer");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    void onPause(){
+        Log.i(TAG, "onPause: observer");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    void onResume(){
+        Log.i(TAG, "onResume: observer");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    void onStart(){
+        Log.i(TAG, "onstart: observer");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    void onStop(){
+        Log.i(TAG, "onStop: observer");
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    void onDestroy(){
+        Log.i(TAG, "onDestroy: observer");
+    }
 }

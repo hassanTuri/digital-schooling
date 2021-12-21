41b961bd8badfe43aa4b757ba9d6451f30cacf65
Haris_cse <muhammadhariskhancsemardan@gmail.com>
2021-12-21 13:55:25 +0500

    i added the class for lifecycle

---
+++ app/src/main/java/com/hassan/fypuidesign/lifecycleaware.java
@@ -0,0 +1,11 @@
+package com.hassan.fypuidesign;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+public class lifecycleaware implements LifecycleObserver {
+
+
+
+}
